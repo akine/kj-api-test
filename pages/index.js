164615c3d296b48889a8e166d9d45e0a531f2a1f
akine <weezer.and.you@gmail.com>
2021-02-14 21:57:01 +0900

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+export default function Home({ sleeps }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      <p>ouraring test</p>
+      {sleeps.map((sleep, index) => (
+        <div key={index}>
+          <h1>{sleep.summary_date}</h1>
+          <p>{sleep.score}</p>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      ))}
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async () => {
+  const Client = require("oura-cloud-api");
+  const accessToken = process.env.ACCESS_TOKEN;
+  const client = new Client(accessToken);
+  const today = new Date();
+  console.log(today);
+  console.log("today");
+
+  const userInfo = await client.getUserInfo();
+  console.log(`The call returned: ${JSON.stringify(userInfo)}`);
+
+  const sleeps = await client.getReadinessSummaries({
+    start: "2019-01-15",
+    end: today,
+  });
+  console.log(`The call returned: ${JSON.stringify(sleeps)}`);
+
+  return {
+    props: {
+      sleeps,
+    },
+  };
+};
